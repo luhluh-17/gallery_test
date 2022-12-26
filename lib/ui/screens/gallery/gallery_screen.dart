57267ebbaaf78883db6bf75c57308899a6e1719c
luhluh-17 <castro.ralph17@gmail.com>
2022-12-26 23:07:45 +0800

    add logout

--- lib/ui/screens/gallery/gallery_screen.dart
+++ lib/ui/screens/gallery/gallery_screen.dart
@@ -12,8 +12,19 @@ class _GalleryScreenState extends State<GalleryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Gallery'),
         centerTitle: true,
+        title: const Text('Gallery'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: const Icon(Icons.logout),
+            ),
+          ),
+        ],
       ),
     );
   }
